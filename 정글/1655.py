fc659e1caf12e9a94dda3694b0145ce188ba2916
hyunS00 <optifun72@gmail.com>
2024-03-27 18:03:31 +0900

    백준 1655 가운데를 말해요 python

--- 정글/1655.py
+++ 정글/1655.py
@@ -5,19 +5,28 @@ N = int(input())
 
 num_list = [int(sys.stdin.readline()) for i in range(N)]
 
-heap = []
+min_heap = []
+max_heap = []
+mid = num_list[0]
+print('시작',mid)
+for i in range(1,N):
+    num = num_list[i]
+    if (i+1) % 2 == 0:
+        # 미드 값이 num보다 작으면 최대힙에 미드 최소 힙에 num
+        if mid < num:
+            heapq.heappush(max_heap,-mid)
+            heapq.heappush(min_heap,num)
+        else:
+            heapq.heappush(min_heap,mid)
+            heapq.heappush(max_heap,-num)
+        mid = -max_heap[0]
 
-for num in num_list:
-    heapq.heappush(heap,num)
-    heap_size = len(heap)+1
-
-    pop_value = []
-    for i in range(heap_size//2):
-        pop_value.append(heapq.heappop(heap))
-
-    print(pop_value[len(pop_value)-1])
-
-    for value in pop_value:
-        heapq.heappush(heap, value)
-    
-    print(heap)
+    else :
+        tmp = None
+        if num < mid:
+            heapq.heappush(max_heap,-num)
+            mid = -heapq.heappop(max_heap)
+        else:
+            heapq.heappush(min_heap,num)
+            mid = heapq.heappop(min_heap)
+    print(mid)
