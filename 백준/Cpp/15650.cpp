246245f8df2c1d62d0709286c83aad51c566cf83
hyunS00 <optifun72@gmail.com>
2024-04-18 11:50:55 +0900

    백준 N과 15650 M(2) cpp

---
+++ 백준/Cpp/15650.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void dfs(vector<int> number, vector<int> visit, int N, int M, int n);
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,M;
+    cin >> N >> M;
+    vector<int> visit(N+1);
+    vector<int> number(M);
+    vector<int> ans;
+    fill(visit.begin(),visit.end(),0);
+
+    dfs(number,visit, N, M, 0);
+}
+
+void dfs(vector<int> number, vector<int> visit,int N, int M, int k){
+
+    if (k == M){
+        for (int i = 0; i < M; i++){
+            cout << number[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int start = k == 0 ? 1 : number[k-1]+1;
+
+    for (int i = start; i <= N; i++){
+        if (visit[i])
+            continue;
+        number[k] = i;
+        visit[i] = 1;
+        dfs(number, visit, N, M, k+1);
+        visit[i] = 0;
+    }
+}
