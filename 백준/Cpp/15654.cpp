fd039a42b784df3f4a6b94ca0c69f1bc1bb7cea3
hyunS00 <optifun72@gmail.com>
2024-04-18 18:51:01 +0900

    백준 15654 N과 M(5) python

---
+++ 백준/Cpp/15654.cpp
@@ -0,0 +1,45 @@
+#include<algorithm>
+#include<vector>
+#include<iostream>
+using namespace std;
+
+void dfs(vector<int> number, vector<int> visit, vector<int> ans, int N, int M, int k);
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> number;
+    vector<int> visit(N);
+    vector<int> ans(N);
+    fill(visit.begin(), visit.end(), 0);
+
+    for (int i=0; i < N; i++){
+        int num;
+        cin >> num;
+        number.push_back(num);
+    }
+
+    sort(number.begin(), number.end());
+
+    dfs(number, visit, ans, N, M, 0);
+}
+
+void dfs(vector<int> number, vector<int> visit, vector<int> ans, int N, int M, int k){
+    if (k == M){
+        for(int i = 0; i < M; i++){
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < N; i++){
+        if (visit[i])
+            continue;
+        visit[i] = 1;
+        ans[k] = number[i];
+        dfs(number, visit, ans, N, M, k+1);
+        visit[i] = 0;
+    }
+}
