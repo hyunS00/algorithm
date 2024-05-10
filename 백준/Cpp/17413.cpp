04e6582936d3a021b4f71bf27d1d96e753c79b10
hyunS00 <optifun72@gmail.com>
2024-05-10 09:53:09 +0900

    백준 17413 단어 뒤집기2 cpp

---
+++ 백준/Cpp/17413.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+stack<char> s;
+string S, ans;
+int is_tag;
+
+vector<string> split(string str, string delimeter){
+    vector<string> ret;
+    long pos = 0;
+    string token;
+    while ((pos = str.find(delimeter)) != string::npos)
+    {
+        token = str.substr(0, pos);
+        ret.push_back(token);
+        str.erase(0, pos + delimeter.length());
+    }
+    ret.push_back(str);
+    return ret;
+}
+int main(){
+    getline(cin, S);
+    vector<string> splitStr = split(S," ");
+
+    for(string str : splitStr){
+        for(char ch : str){
+            if(is_tag){
+                ans += ch;
+                if(ch == '>')
+                    is_tag = 0;
+            }
+            else if (ch == '<'){
+                while (!s.empty())
+                {
+                    ans += s.top();
+                    s.pop();
+                }
+                ans += '<';
+                is_tag = 1;
+            }
+            else{
+                s.push(ch);
+            }
+        }
+        while (!s.empty()){
+            ans += s.top();
+            s.pop();
+        }
+        ans += ' ';
+    }
+    cout << ans << "\n";
+}
