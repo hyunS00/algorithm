217d66cf3c6967d014da6143a6384d898f9deb4a
hyunS00 <optifun72@gmail.com>
2024-05-21 12:12:25 +0900

    백준 1373 2진수 8진수 cpp

---
+++ 백준/Cpp/1373.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+string binary;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> binary;
+
+    while (binary.length() % 3 != 0)
+    {
+        binary = '0' + binary;
+    }
+    cout << binary << "\n";
+    for(int i = 0; i < binary.length(); i += 3){
+        int num = (binary[i]-'0') * 4 + (binary[i+1] - '0') * 2 + (binary[i+2] - '0');
+        cout << num;
+    }
+    cout << "\n";
+}
