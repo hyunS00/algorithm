4c659b28edde40fe7302c64b66eec20df9ada9eb
hyunS00 <optifun72@gmail.com>
2024-06-05 10:36:35 +0900

    백준 21598 SciComLove cpp

---
+++ 백준/Cpp/21598.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define endl "\n"
+
+using namespace std;
+
+int N;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N;
+    
+    while(N--) {
+        cout << "SciComLove" << endl;
+    }
+}
