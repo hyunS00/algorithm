2faf7a6e6b5f6f290afdc08adb90ac15f6e958fe
hyunS00 <optifun72@gmail.com>
2024-08-05 11:21:20 +0900

    [백준 2441] 별 찍기-4 - cpp

---
+++ 백준/Cpp/2441.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define endl "\n"
+
+using namespace std;
+
+int n;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin >> n;
+
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+
+        for(int j = 0; j < n-i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
