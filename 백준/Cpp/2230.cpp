b2f61282d7929c9be6c712de47f5366d782329ce
hyunS00 <optifun72@gmail.com>
2024-09-30 12:36:35 +0900

    [백준 2230] 수 고르기 - cpp

---
+++ 백준/Cpp/2230.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+
+int n, m;
+int a[100005];
+int mn = 0x7fffffff;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, a+n);
+    int st = 0;
+    int en = 0;
+
+
+    for(int st = 0; st < n; st++){
+        while (en < n && a[en] - a[st] < m) en++;
+        if(en == n) break;
+        mn = min(mn, a[en] - a[st]);
+        
+    }
+    cout << mn;
+}
