5ee0cbff15c01873304bbdd53541b74dcb00b952
hyunS00 <optifun72@gmail.com>
2024-10-06 12:37:39 +0900

    [백준 7662] 이중 우선순위 큐 - cpp

--- 백준/Cpp/7662.cpp
+++ 백준/Cpp/7662.cpp
@@ -5,17 +5,13 @@ using namespace std;
 
 int t;
 
-
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> t;
-
     while (t--) {
         int k;
         multiset<int> ms;
-
         cin >> k;
-
         while (k--) {
             char c;
             int n;
@@ -25,9 +21,7 @@ int main(){
             else if(n == 1) ms.erase(prev(ms.end()));
             else if(n == -1) ms.erase(ms.begin());
         }
-
         if(ms.empty()) cout << "EMPTY" << endl;
         else cout << *prev(ms.end()) <<  " " << *ms.begin() <<  endl;;
     }
-    
 }
