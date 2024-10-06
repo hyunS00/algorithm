b61c86c985217fa18f24410f1557ae488a45b75e
hyunS00 <optifun72@gmail.com>
2024-10-06 12:37:15 +0900

    [백준 7662] 이중 우선순위 큐큐 - cpp

---
+++ 백준/Cpp/7662.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<set>
+#define endl "\n"
+using namespace std;
+
+int t;
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> t;
+
+    while (t--) {
+        int k;
+        multiset<int> ms;
+
+        cin >> k;
+
+        while (k--) {
+            char c;
+            int n;
+            cin >> c >> n;
+            if(c == 'I') ms.insert(n);
+            else if(ms.empty()) continue;
+            else if(n == 1) ms.erase(prev(ms.end()));
+            else if(n == -1) ms.erase(ms.begin());
+        }
+
+        if(ms.empty()) cout << "EMPTY" << endl;
+        else cout << *prev(ms.end()) <<  " " << *ms.begin() <<  endl;;
+    }
+    
+}
