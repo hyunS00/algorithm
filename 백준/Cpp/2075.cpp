4912abea85101f94303c112bd846b5e3b3fa1d0c
hyunS00 <optifun72@gmail.com>
2024-10-07 17:08:26 +0900

    [백준 2075] N번째 큰 수

---
+++ 백준/Cpp/2075.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n;
+priority_queue<int> PQ;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            int num;
+            cin >> num;
+            PQ.push(-num);
+
+            if(PQ.size() > n) PQ.pop();
+        }
+    }
+
+    cout << -PQ.top();
+}
