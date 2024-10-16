5b0a13864d37cc4ceb085333f590786b0a593313
hyunS00 <optifun72@gmail.com>
2024-10-16 13:19:54 +0900

    [백준 9372] 상근이의 여행 - cpp

---
+++ 백준/Cpp/9372.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#define endl "\n"
+using namespace std;
+
+int t, n, m;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+
+        for(int e = 0; e < m; e++){
+            int a, b;
+            cin >> a >> b;
+        }
+
+        cout << n-1 << endl;
+    }
+}
